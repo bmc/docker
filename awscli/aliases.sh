d36a79b9aed1c9db3d6e6e9e1c258f2f777451c7
Brian Clapper <bmc@clapper.org>
2019-03-07 14:46:15 -0500

    Prefix image name with bclapper/

--- awscli/aliases.sh
+++ awscli/aliases.sh
@@ -3,5 +3,5 @@ function aws {
   # container ("user").
   docker_home=$(echo $HOME | sed -E -e "s|/$USER(/?)|/user\1|")
   docker_pwd=$(pwd | sed -E -e "s|/$USER(/?)|/user\1|")
-  docker run -it --rm -w $docker_pwd -v $HOME:$docker_home -e TERM=xterm awscli aws "$@"
+  docker run -it --rm -w $docker_pwd -v $HOME:$docker_home -e TERM=xterm bclapper/awscli aws "$@"
 }
--- awscli/install.sh
+++ awscli/install.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker build -t awscli .
+docker build -t bclapper/awscli .
