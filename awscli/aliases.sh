a4ef83f07afbe6b684a024bd623de59697e05bad
Brian Clapper <bmc@clapper.org>
2019-05-10 20:40:50 -0400

    Added groff to Docker container. Added PAGER, LESS and LESSCHARSET environment variables to docker invocation.

--- awscli/aliases.sh
+++ awscli/aliases.sh
@@ -3,5 +3,10 @@ function aws {
   # container ("user").
   docker_home=$(echo $HOME | sed -E -e "s|/$USER(/?)|/user\1|")
   docker_pwd=$(pwd | sed -E -e "s|/$USER(/?)|/user\1|")
-  docker run -it --rm -w $docker_pwd -v $HOME:$docker_home -e TERM=xterm bclapper/awscli aws "$@"
+  docker run -it --rm -w $docker_pwd -v $HOME:$docker_home \
+    -e TERM=xterm \
+    -e PAGER="$PAGER" \
+    -e LESS="$LESS" \
+    -e LESSCHARSET="$LESSCHARSET" \
+    bclapper/awscli aws "$@"
 }
