309c77738407c8179ef4cb00208657cfe11df520
Brian Clapper <bmc@clapper.org>
2019-08-28 17:31:49 -0400

    Force :latest

--- nbconvert/aliases.sh
+++ nbconvert/aliases.sh
@@ -2,12 +2,12 @@ function nbconvert {
   docker_home=$(echo $HOME | sed -E -e "s|/$USER(/?)|/user\1|")
   docker run -it --rm -w `pwd` -v $HOME:$HOME \
              -e TERM=xterm -e HOME=$docker_home -e USER=user \
-             bclapper/nbconvert jupyter nbconvert "$@"
+             bclapper/nbconvert:latest jupyter nbconvert "$@"
 }
 
 function nbconvertbash {
   docker_home=$(echo $HOME | sed -E -e "s|/$USER(/?)|/user\1|")
   docker run -it --rm -w `pwd` -v $HOME:$HOME \
              -e TERM=xterm -e HOME=$docker_home -e USER=user \
-             bclapper/nbconvert /bin/bash "$@"
+             bclapper/nbconvert:latest /bin/bash "$@"
 }
