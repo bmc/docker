c63ef29f705b4cf16cd64d2d6f09b2bd4d4ffe21
Brian Clapper <bmc@clapper.org>
2020-01-13 12:02:34 -0500

    Allow 2.7 and 2.9 Pandoc to be invoked.

--- pandoc/aliases.sh
+++ pandoc/aliases.sh
@@ -1,3 +1,7 @@
-function pandoc {
-   docker run --rm -v `pwd`:/data --user `id -u`:`id -g` pandoc/latex:2.9 "$@"
+alias pandoc=pandoc29
+alias pandoc29="PANDOC_VERSION=2.9 run_pandoc"
+alias pandoc27="PANDOC_VERSION=2.7 run_pandoc"
+
+function run_pandoc {
+  docker run --rm -v `pwd`:/data --user `id -u`:`id -g` pandoc/latex:$PANDOC_VERSION "$@"
 }
